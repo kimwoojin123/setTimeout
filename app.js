3e37eb1fbb4bc0f9e3955a9d611efaa144c078c8
kimwoojin <kimwoojin1903@gmail.com>
2023-10-06 11:11:17 +0900

    setTimeout 내부에 setTimeout이 들어갔을때 실험

---
+++ app.js
@@ -0,0 +1,9 @@
+setTimeout(function () {
+    console.log('첫번째 소사');
+    setTimeout(function () {
+        console.log('두번째 문기');
+        setTimeout(function () {
+            console.log('세번째 민구');
+        }, 1.0 * 1000);
+    }, 2.0 * 1000);
+}, 3.0 * 1000);
