f750a88a5dad809378b0730b31d8d024afed5709
kimwoojin <kimwoojin1903@gmail.com>
2023-10-06 11:29:52 +0900

    setInterval 뜯어보기

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-setTimeout(function () {
-    console.log('첫번째 소사');
+// setTimeout(function () {
+//     console.log('첫번째 소사');
+//     setTimeout(function () {
+//         console.log('두번째 문기');
+//         setTimeout(function () {
+//             console.log('세번째 민구');
+//         }, 1.0 * 1000);
+//     }, 2.0 * 1000);
+// }, 3.0 * 1000);
+
+function sosa() {
     setTimeout(function () {
-        console.log('두번째 문기');
-        setTimeout(function () {
-            console.log('세번째 민구');
-        }, 1.0 * 1000);
-    }, 2.0 * 1000);
-}, 3.0 * 1000);
+        console.log('소사');
+        sosa();
+    }, 1000);
+}
+
+sosa();
